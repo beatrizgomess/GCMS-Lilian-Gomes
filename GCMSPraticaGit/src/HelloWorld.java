7bf23992b90ac395445a5e1f17aea5ef0b94b13c
bea.gomes <beatrizgomesxx@gmail.com>
2023-05-03 21:29:15 -0300

    pratica 02

---
+++ GCMSPraticaGit/src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Estudo no IFPE");
+		System.out.println("Curso TADS");
+	
+		
+	}
+
+}
