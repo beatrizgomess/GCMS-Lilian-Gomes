21c9658ed4c8fd1c89c700345681c01bfa3821b0
bea.gomes <beatrizgomesxx@gmail.com>
2023-05-08 11:25:51 -0300

    resolvendo os conflitos

--- GCMSPraticaGit/src/HelloWorld.java
+++ GCMSPraticaGit/src/HelloWorld.java
@@ -10,7 +10,9 @@ public class HelloWorld {
 		System.out.println("DISCIPLINA: Gerência de Configuração e Mudança de Software");
 	
 		System.out.println("Adicionando Modificações ");
-		System.out.println("Modificações Referente a pratica 04 - Trabalhando NA BRANCH HOTFIX");
+		System.out.println("Modificações Referente a pratica 04");
+		System.out.println("Trabalhando NA BRANCH HOTFIX");
+
 		
 
 		
